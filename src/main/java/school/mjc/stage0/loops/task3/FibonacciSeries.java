8cb9e30e1ec977f5bb885f8a57a2c3e64c339dca
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 08:08:11 +0500

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -1,6 +1,26 @@
 package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
+
     public void printFibonacci(int lastFibonacci) {
+
+        int first = 0;
+        int second = 1;
+        int third=0;
+
+        System.out.println(first);
+        System.out.println(second);
+
+        for (int i = 2; i <lastFibonacci; i++) {
+
+            third=first+second;
+            first=second;
+            second=third;
+
+
+            System.out.println(third);
+
+
+        }
     }
 }
