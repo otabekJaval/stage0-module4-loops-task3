a84e4699621cc58ff17908af92010ed77cd060a7
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 08:07:57 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,15 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
 
+    public  void printDigitsSum(int t){
+
+        t=Math.abs(t);
+        int sum=0;
+        while (t>0){
+            sum+=t%10;
+            t/=10;
+        }
+        System.out.println(sum);
     }
 }
