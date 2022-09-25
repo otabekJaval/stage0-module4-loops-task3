8c599a7c2646dcab1578d0ee2f2d2b214ff2c91b
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 08:07:37 +0500

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
+
     public void printAlphabet() {
+
+        for (int i = 97; i < 123; i++) {
+            System.out.println((char) i);
+        }
     }
 }
