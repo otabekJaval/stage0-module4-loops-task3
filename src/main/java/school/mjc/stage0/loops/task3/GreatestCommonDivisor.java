ee46615868abc7ed45987131f2ae41048c9c132a
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 08:08:25 +0500

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
-    public void printGCD(int first, int second) {
+    public  void printGCD(int first, int second) {
+
+        int n=Math.max(first,second);
+
+        while (!(first%n==0&&second%n==0)){
+            n--;
+        }
+        System.out.println(n);
     }
 }
